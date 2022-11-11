95446ff89e64648181f1fe4756b2ce232bbaa784
Shalini 1908 <989163962g@gmail.com>
2022-11-11 09:30:03 +0530

    added mumbai

---
+++ Data/data2.js
@@ -0,0 +1,65 @@
+let productData = [
+    {
+        "Title": "Lemon Tree Premier, Mumbai International Airport, Mumbai",
+        "Image": "https://r2imghtlak.mmtcdn.com/r2-mmt-htl-image/htl-imgs/201905141416461036-348d3ee68de111e9b0ba0242ac11000b.jpg?&output-quality=75&downsize=910:612&crop=910:612;3,0&output-format=jpg&downsize=821:550&crop=821:550",
+        "Price": 3700,
+    },
+
+    {
+        "Title": "The Lalit Mumbai",
+        "Image": "https://r1imghtlak.mmtcdn.com/249d44e6719511e7a5c1025f77df004f.jpg?&output-quality=75&downsize=910:612&crop=910:612;4,0&output-format=jpg&downsize=821:550&crop=821:550",
+        "Price": 1500,
+
+    },
+    {
+        "Title": "The Orchid Hotel Mumbai Vile Parle",
+        "Image": "https://r1imghtlak.mmtcdn.com/df1cbb06b25b11e7aedf02f506bf1356.jpg?&output-quality=75&downsize=910:612&crop=910:612;79,0&output-format=jpg&downsize=821:550&crop=821:550",
+        "Price": 2800,
+
+    },
+    {
+        "Title": "The Avenue,Calangute",
+        "Image": "https://r1imghtlak.mmtcdn.com/071f7230bc0911e884a00aceac5c031a.jpg?&output-quality=75&downsize=910:612&crop=910:612;157,0&output-format=jpg&downsize=821:550&crop=821:550",
+        "Price": 2400,
+
+    },
+    {
+        "Title": "Trident Bandra Kurla",
+        "Image": "https://r1imghtlak.mmtcdn.com/17188b222dba11e99e330242ac110004.jpg?&output-quality=75&downsize=910:612&crop=910:612;55,0&output-format=jpg&downsize=821:550&crop=821:550",
+        "Price": 2250,
+
+    },
+    {
+        "Title": "Sahara Star",
+        "Image": "https://r2imghtlak.mmtcdn.com/r2-mmt-htl-image/htl-imgs/200709211000561392-52a39cdec78411ebaca20242ac110005.jpg?&output-quality=75&downsize=910:612&crop=910:612;218,0&output-format=jpg&downsize=821:550&crop=821:550",
+         "Price": 1000,
+
+    },
+    {
+        "Title": "Ginger Mumbai Andheri",
+        "Image": "https://r1imghtlak.mmtcdn.com/8e8c8f60780c11e7b2b5025f77df004f.jpg?&output-quality=75&downsize=910:612&crop=910:612;4,0&output-format=jpg&downsize=821:550&crop=821:550",
+         "Price": 3200,
+
+    },
+    {
+        "Title": "Ramada Plaza By Wyndham Palm Grove, Juhu, Mumbai",
+        "Image": "https://r1imghtlak.mmtcdn.com/d154bc84f52311e78080025f77df004f.jpg?&output-quality=75&output-format=jpg&downsize=821:550&crop=821:550",
+         "Price": 2100,
+
+    },
+    {
+        "Title": "ITC Maratha - A Luxury Collection Hotel",
+        "Image": "https://r1imghtlak.mmtcdn.com/87402574c37f11eb84af0242ac110002.jpg?&output-quality=75&downsize=910:612&crop=910:612;58,0&output-format=jpg&downsize=821:550&crop=821:550",
+        "Price": 3200,
+
+    },
+    {
+        "Title": "Ramee Techome",
+        "Image": "https://r1imghtlak.mmtcdn.com/653ebe1ce36c11e999470242ac110005.jpg?&output-quality=75&downsize=910:612&crop=910:612;4,0&output-format=jpg&downsize=821:550&crop=821:550",
+         "Price": 3000,
+
+    }
+]
+
+
+  export default productData

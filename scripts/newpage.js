de2dba977f77ad3df22d420a9f7f0cbad04406b3
hrehankhan <rk46977@gmail.com>
2022-11-12 16:57:43 +0530

    day-5 complete rehan

---
+++ scripts/newpage.js
@@ -0,0 +1,41 @@
+import getnavbar from "../components/navbar.js";
+
+let nav = document.getElementById("navbarImp");
+nav.innerHTML = getnavbar();
+
+let signupls = JSON.parse(localStorage.getItem("signupData")) || []
+
+let newB_btn = document.getElementById("newB").addEventListener("click",signedup)
+function signedup() {
+    let name = document.getElementById("firstN").value;
+    let lastname = document.getElementById("lastN").value;
+    let mobile = document.getElementById("newMob").value;
+    let email = document.getElementById("newMail").value;
+    let pass = document.getElementById("newPass").value;
+
+
+    if(name == "" || lastname == "" || email == "" || pass == "" || mobile == ""){
+        return;
+    }else{
+        let obj = {
+            name,
+            lastname,
+            mobile,
+            email,
+            pass
+        }
+    signupls.push(obj);
+
+    localStorage.setItem("signupData",JSON.stringify(signupls));
+
+    window.location.href="./newsignin.html";
+    
+    }
+    
+    document.getElementById("firstN").value = null;
+    document.getElementById("lastN").value = null;
+    document.getElementById("newMob").value = null;
+    document.getElementById("newMail").value = null;
+    document.getElementById("newPass").value = null;
+
+}
